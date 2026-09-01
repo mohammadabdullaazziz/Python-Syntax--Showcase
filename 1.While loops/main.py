a474f860d9c2c93bca73527bcf6d682b1267324e
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-09-01 14:04:40 +0600

    Code Push

--- 1.While loops/main.py
+++ 1.While loops/main.py
@@ -90,7 +90,26 @@ while count < 10: পাইথন চেক করে দেখল count-এর 
 
 
 
+# count = 0
 
+# start = False
+
+# while count < 5 or not start:
+#     user_input = input("Enter 'start' to begin or 'exit' to quit: ").strip().lower()
+    
+#     if user_input == 'start':
+#       start = True
+      
+      
+#       print("Starting the process...")
+#     elif user_input == 'exit':
+#       print("Exiting the program.")
+#       break
+#     else:
+#       print("Invalid input. Please try again.")
+    
+#     count += 1
+  
 
 
   
